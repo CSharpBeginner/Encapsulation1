1653dbd7233f35eb7212de5dd2eabaab15a08bfa
CSharpBeginner <Mekhonoshin.Pavel@yandex.ru>
2022-01-31 19:33:40 +0500

    Validation
    1.Add constructor
    2.Add values validation

--- Weapon.cs
+++ Weapon.cs
@@ -2,14 +2,47 @@ public class Weapon
 {
     private int _damage;
     private int _bullets;
+    private int _bulletsPerShoot;
 
-    public void Fire(Player player)
+    public Weapon(int damage, int bullets, int bulletsPerShoot)
     {
-        if (_bullets >= 1)
+        if (damage < 0)
         {
-            player.TakeDamage(_damage);
-            _bullets -= 1;
+            throw new ArgumentOutOfRangeException(nameof(damage));
         }
+
+        if (bullets < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(bullets));
+        }
+
+        if (bulletsPerShoot < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(bulletsPerShoot));
+        }
+
+        _damage = damage;
+        _bullets = bullets;
+        _bulletsPerShoot = bulletsPerShoot;
+    }
+
+    public void TryFire(Player player)
+    {
+        if (player == null)
+        {
+            throw new ArgumentNullException(nameof(player));
+        }
+
+        if (_bullets >= _bulletsPerShoot)
+        {
+            Fire(Player player);
+        }
+    }
+
+    private void Fire(Player player)
+    {
+        player.TakeDamage(_damage);
+        _bullets -= _bulletsPerShoot;
     }
 }
 
@@ -17,8 +50,23 @@ public class Player
 {
     private int _health;
 
+    public Player(int health)
+    {
+        if (health <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(health));
+        }
+
+        _health = health;
+    }
+
     public void TakeDamage(float damage)
     {
+        if (damage < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(damage));
+        }
+
         if (_health - damage > 0)
         {
             _health -= damage;
@@ -34,8 +82,18 @@ public class Bot
 {
     private Weapon _weapon;
 
+    public Bot(Weapon weapon)
+    {
+        _weapon = weapon;
+    }
+
     public void OnSeePlayer(Player player)
     {
-        _weapon.Fire(player);
+        if (_weapon == null)
+        {
+            throw new ArgumentNullException(nameof(_weapon));
+        }
+
+        _weapon.TryFire(player);
     }
 }
